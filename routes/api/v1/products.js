6cf354e9200c04bb488f45b46eae7cfda985367a
admin <Garalax>
2020-08-15 00:36:26 +0700

    PUSH for making search product api

--- routes/api/v1/products.js
+++ routes/api/v1/products.js
@@ -27,11 +27,53 @@ router.get('/:id?', function(req, res, next) {
         result: req.params.id
     });
 });
-router.post('/', function(req, res, next) {
-    res.json({
-        success: true,
-        result: "This is Post method"
-    });
+router.post('/search', async function(req, res, next) {
+    var filter
+    const body = req.body
+    if(body.filters.length > 0) {
+      filter = body.filters
+    } else {
+      const filterTemp = []
+      filterTemp.push('fruit')
+      filterTemp.push('vegetables')
+      filterTemp.push('dairy')
+      filterTemp.push('meat')
+      filterTemp.push('Seafood')
+      filterTemp.push("Wheat & Grain")
+      filter = filterTemp
+    }
+    const snapshot = await db.collection('product')
+    .where('price', '<=', body.priceMax)
+    .where('price', '>=', body.priceMin)
+    .where('category','in', filter)
+    .get();
+    if (snapshot.empty) {
+        res.send("NO SERVERS AVAILABLE");
+    } else {
+      const result = []
+      var docs = snapshot.docs.map(doc => doc.data())
+      docs.forEach( doc => {
+        if(doc.name.toLowerCase().includes(body.keyword.toLowerCase())) {
+          result.push(doc)
+        }
+      })
+      if(result.length > 0) {
+        res.end(JSON.stringify(
+          {
+            status: 200,
+            success: true,
+            data: result
+          }
+        ));
+      } else {
+        res.end(JSON.stringify(
+          {
+            status: 200,
+            success: false
+          }
+        ));
+      }
+    }
   });
 
   router.post('/item/', function(req, res, next) {
